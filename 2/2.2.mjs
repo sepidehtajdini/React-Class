6410ff8622ed4a07dda93abb6d57d32bf9dbec0d
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-12 07:58:01 -0800

    Add files via upload

---
+++ 2/2.2.mjs
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import chalk from "chalk";
+const id = process.argv[2];
+axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(function (json) {
+        const title = json.data.title
+        const comp = json.data.completed;
+
+        if (comp === true) {
+            console.log(chalk.green(title))
+        } else {
+            console.log(chalk.red(title))
+        }
+    });
---
+++ 2/2.3.mjs
@@ -0,0 +1,3 @@
+import { square, power } from "./my_modules.mjs";
+let number = prompt("Please enter the number");
+alert(`Square root:${square(number)} and Power:${power(number)}`);
