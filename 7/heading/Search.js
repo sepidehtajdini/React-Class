1ba6816d60fd8e5ea5a65c0bdcc85f3756c00c24
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-13 14:15:30 +0330

    Update Search.js

--- 7/heading/Search.js
+++ 7/heading/Search.js
@@ -2,16 +2,10 @@ import Button from "../shared/Button";
 import "./heading.css";
 
 function Search({ search, inp }) {
-
-    function onKeyDown() {
-        window.addEventListener('keydown', (event) => {
-            document.querySelector('input').focus();
-        });
-    }
     return (
         <div className="header__bar__searchBar">
             <input ref={inp} placeholder="متن برای جستجو" name="searchBox" autoFocus
-                onKeyDown={onKeyDown} />
+                onKeyDown={() => inp.focus()} />
             <Button
                 type="button"
                 onClick={search}
