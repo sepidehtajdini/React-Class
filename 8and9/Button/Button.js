f033d07e9f9a3b3a32ec26987d04af7a3ebcf2d2
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-20 22:39:42 +0330

    Delete 8and9/Button directory

--- 8and9/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import "./button.css";
-function Button({text,className,onClick}){
-    return(
-        <button className={className} onClick={onClick}>{text}</button>
-    )
-}
-export default Button
