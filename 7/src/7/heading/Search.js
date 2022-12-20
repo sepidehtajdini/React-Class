7267ea838f4fce080e2bcafb6ec1569d87294254
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-20 23:09:52 +0330

    Add files via upload

---
+++ 7/src/7/heading/Search.js
@@ -0,0 +1,17 @@
+import Button from "../shared/Button";
+import "./heading.css";
+function Search({ search, inp }) {
+    return (
+        <div className="header__bar__searchBar">
+            <input ref={inp} placeholder="متن برای جستجو" name="searchBox" autoFocus
+                onKeyDown={() => inp.focus()} />
+            <Button
+                type="button"
+                onClick={search}
+                className="secondary_btn"
+                text="جستجو"
+            />
+        </div>
+    )
+}
+export default Search
