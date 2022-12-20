e1fe8e71b1f5557392ac1f23b2efad94398dcdda
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-20 23:03:54 +0330

    Delete 7/shared directory

--- 7/shared/Button.js
+++
@@ -1,9 +0,0 @@
-import "./button.css"
-function Button({ text, type, className, onClick }) {
-    return (
-        <div>
-            <button type={type} className={`btn ${className}`} onClick={onClick}>{text}</button>
-        </div>
-    )
-}
-export default Button
